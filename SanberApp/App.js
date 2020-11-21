f3eb14ed8895a4179219638c58bf9a046b4cc73b
umarjabbar <umarjuabbar@gmail.com>
2020-11-21 22:27:18 +0700

    Tugas 14 - Maaf telat:3

--- SanberApp/App.js
+++ SanberApp/App.js
@@ -5,22 +5,15 @@ import YoutubeUi from './Tugas/Tugas12.js/App';
 import ToggleIndex from './Latihan/week3-3/index';
 import LoginScreen from './Tugas/Tugas13/LoginScreen'
 import AboutScreen from './Tugas/Tugas13/AboutScreen'
+import Main from './Tugas/Tugas14/components/Main';
 export default function App() {
   return (
     <>
       {/* <YoutubeUi></YoutubeUi> */}
       {/* <ToggleIndex /> */}
       {/* <LoginScreen/> */}
-      <AboutScreen/>
+      {/* <AboutScreen/> */}
+      <Main/>
     </>
     );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
